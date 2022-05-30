c3390eeed1b08ac6cc4a54ace810876288b93d66
unknown <lukacc008@gmail.com>
2022-05-30 16:15:19 +0200

    Did the same thing for password states, and replaced validation state with old but updated code of useEffect hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -47,35 +47,36 @@ const Login = (props) => {
     };
   }, []);
 
-  /*
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Cheking form validity!");
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
 
     return () => {
       console.log("CLEANUP");
       clearTimeout(identifier);
     };
-  }, [enteredEmail, enteredPassword]);
-  */
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
-    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value })
+  };
 
+    /*
     setFormIsValid(
       event.target.value.includes("@") && passwordState.isValid
     );
   };
-
+    */
   const passwordChangeHandler = (event) => {
     dispatchPassword({type:"USER_INPUT", val: event.target.value})
-
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
+
+  //  setFormIsValid(emailState.isValid && event.target.value.trim().length > 6); };
 
   const validateEmailHandler = () => {
     dispatchEmail({ type: "INPUT_BLUR" });
